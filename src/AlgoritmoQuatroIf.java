b99f12f155746e8528b13f98be9611abe7201d53
danielneiva-design <danielneiva.design@gmail.com>
2026-09-08 08:35:58 -0300

    Novos exercícios de if - Algoritmo Quartro If

---
+++ src/AlgoritmoQuatroIf.java
@@ -0,0 +1,15 @@
+package src;
+
+public class AlgoritmoQuatroIf {
+    void main() {
+        //entre com um número inteiro e mostre se ele é positivo, negativo ou nulo
+        int n = Integer.parseInt(IO.readln("entre com um número: "));
+        if (n > 0) {
+            System.out.println("Número positivo");
+        } else if (n < 0) {
+            System.out.println("Número negativo");
+        } else {
+            System.out.println("Número nulo");
+        }
+    }
+}
