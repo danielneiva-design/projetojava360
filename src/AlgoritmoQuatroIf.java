997c2ce1ab99d28e1d00d5c6250bdbf8d4649488
danielneiva-design <danielneiva.design@gmail.com>
2026-09-08 08:46:06 -0300

    Algoritmo Quatro If atualizado

--- src/AlgoritmoQuatroIf.java
+++ src/AlgoritmoQuatroIf.java
@@ -5,11 +5,11 @@ public class AlgoritmoQuatroIf {
         //entre com um número inteiro e mostre se ele é positivo, negativo ou nulo
         int n = Integer.parseInt(IO.readln("entre com um número: "));
         if (n > 0) {
-            System.out.println("Número positivo");
+            IO.println("Número positivo");
         } else if (n < 0) {
-            System.out.println("Número negativo");
+            IO.println("Número negativo");
         } else {
-            System.out.println("Número nulo");
+            IO.println("Número nulo");
         }
     }
 }
