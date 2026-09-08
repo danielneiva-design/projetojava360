e7b28650f17afe93becf40d8527ea1acc72d50a6
danielneiva-design <danielneiva.design@gmail.com>
2026-09-08 09:12:29 -0300

    Atualização nova - Algoritmo Quatro If

--- src/AlgoritmoQuatroIf.java
+++ src/AlgoritmoQuatroIf.java
@@ -11,5 +11,22 @@ public class AlgoritmoQuatroIf {
         } else {
             IO.println("Número nulo");
         }
+
+    //operador ternário
+        int n1 = Integer.parseInt(IO.readln("entre com um número: "));
+        //? significa "então" e : significa "senão"
+        String resultado = (n1 > 0) ? "Número positivo" : (n1 < 0) ? "Número negativo" : "Número nulo";
+        IO.println(resultado);
+    
+        //estrutura de seleção múltipla
+        int numAula = Integer.parseInt(IO.readln("entre com um número de 1 a 5 para escolher sua Aula: "));
+        switch (numAula) {
+            case 1 -> IO.println("Aula 1 - segunda-feira");
+            case 2 -> IO.println("Aula 2 - terça-feira");
+            case 3 -> IO.println("Aula 3 - quarta-feira");
+            case 4 -> IO.println("Aula 4 - quinta-feira");
+            case 5 -> IO.println("Aula 5 - sexta-feira");
+            default -> IO.println("Não temos aula nesse dia.");
+        }
     }
-}
+}    
