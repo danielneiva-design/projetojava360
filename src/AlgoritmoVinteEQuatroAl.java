08f3e0d2cbb5edcb8e8ede08a8c05aed3707bc88
danielneiva-design <danielneiva.design@gmail.com>
2026-09-14 02:18:23 -0300

    Algoritmo 24 - Aplicando a ideia dos vetores

---
+++ src/AlgoritmoVinteEQuatroAl.java
@@ -0,0 +1,19 @@
+package src;
+
+public class AlgoritmoVinteEQuatroAl {
+    public void main() {
+        //Definindo um vetor de 5 posições
+        int[] numeros = new int[5];
+
+        //Preenchimento do vetor com os números digitados pelo usuário
+        for (int i = 0; i< numeros.length; i++){
+            numeros[i] = Integer.parseInt(IO.readln("Entre com o número desejado:"));
+        }
+
+        //Exibição dos números digitados
+        IO.println("=== NÚMEROS DIGITADOS ===");
+        for (int i = 0; i< numeros.length; i++){
+            IO.println("Número " + (i+1) + ": " + numeros[i]);
+        }
+    }
+}
