6bb2273a55c5d2022b5d3f3e035519e1f9e80bd5
danielneiva-design <danielneiva.design@gmail.com>
2026-09-14 09:54:22 -0300

    Algoritmo 28 - Escolher números pares do vetor e 29 incompleto

---
+++ src/AlgoritmoVinteENoveAl.java
@@ -0,0 +1,8 @@
+package src;
+
+public class AlgoritmoVinteENoveAl {
+    public void main(){
+        //Crie um algoritmo que armazene as vendas do ano em um vetor e mostre o total da venda anual e a média anual.
+        
+    }
+}
---
+++ src/AlgoritmoVinteEOitoAl.java
@@ -0,0 +1,21 @@
+package src;
+
+public class AlgoritmoVinteEOitoAl {
+    public void main(){
+        // definindo o vetor de 5 posições
+        int[] numeros = new int[5];
+
+        // preenchendo o vetor com os números digitados pelo usuário
+        for(int i = 0; i < numeros.length; i++){
+            numeros[i] = Integer.parseInt(IO.readln("entre com o número: "));
+        }
+
+        //exibir apenas os números pares
+          IO.println("=== NÚMEROS PARES ENCONTRADOS ===");
+        for(int i = 0; i < numeros.length; i++){
+            if(numeros[i] % 2 == 0){
+                IO.println("Posição " + (i+1) + " = " + numeros[i]);
+            }
+        }
+    }
+}
