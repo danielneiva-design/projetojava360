65f85b337812d300c752012d1f7ed33e3f6457b7
danielneiva-design <danielneiva.design@gmail.com>
2026-09-14 10:16:43 -0300

    Algoritmo 29 - Calculo de média mensal de vendas por mês

--- src/AlgoritmoVinteENoveAl.java
+++ src/AlgoritmoVinteENoveAl.java
@@ -3,7 +3,17 @@ package src;
 public class AlgoritmoVinteENoveAl {
     public void main(){
         //Crie um algoritmo que armazene as vendas do ano em um vetor e mostre o total da venda anual e a média anual.
-        
+        int[] vendas = new int[12];
 
+        double totalVendas = 0;
+
+        for (int i = 0; i < vendas.length; i++) {
+            vendas[i] = Integer.parseInt(IO.readln("Entre com o valor da venda do mês " + (i + 1) + ": "));
+            totalVendas += vendas[i];
+        }
+
+        double mediaVendas = (double) totalVendas / vendas.length;
+        System.out.printf("Total de vendas do ano: R$ %.2f%n", totalVendas);
+        System.out.printf("Média mensal de vendas: R$ %.2f%n", mediaVendas);
     }
 }
