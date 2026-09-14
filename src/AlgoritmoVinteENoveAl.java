55388e10a58fcfbe18a42ac67ec778aed492ff51
danielneiva-design <danielneiva.design@gmail.com>
2026-09-14 10:02:40 -0300

    29 teste

--- src/AlgoritmoVinteENoveAl.java
+++ src/AlgoritmoVinteENoveAl.java
@@ -4,5 +4,6 @@ public class AlgoritmoVinteENoveAl {
     public void main(){
         //Crie um algoritmo que armazene as vendas do ano em um vetor e mostre o total da venda anual e a média anual.
         
+
     }
 }
