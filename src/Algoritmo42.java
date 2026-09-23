2699ef51b125a7d1cb539abfaa0ac57c9826aae2
danielneiva-design <danielneiva.design@gmail.com>
2026-09-23 11:43:16 -0300

    Algoritmo 42 - Criação de lista utilizando List.of

---
+++ src/Algoritmo42.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Algoritmo42 {
+    public void main(){
+        //JAVA 5
+        //James Gosling
+        //Coleção/Coleções (figurinhas)
+        //Collections
+        //Antes do JAVA 5 - calça normal
+        //A partir do JAVA 5 - calça de lycra
+        //Primeira voz (Bruno): Interface (contrato)
+        //Segunda voz (Marrone): Classe (implementa)
+        //List<String> nomes = new ArrayList<>();
+
+        List<String> linguagens = List.of("Rust", "Python", "GO", "JAVA", "C", "C++", "C#");
+        for(String linguagem : linguagens){
+            IO.println(linguagem);
+        }
+
+
+    }
+}
